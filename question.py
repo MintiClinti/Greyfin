2e544066959d07298a1bdfe5531878a20a9802cf
Clinton Nguyen <clintonnguyen@gmail.com>
2024-03-23 11:59:31 -0700

    question type

--- question.py
+++ question.py
@@ -1,10 +1,16 @@
 
-questions = ["What specific needs do you have? (medication, companionship, etc) ",
-            "What is your caretaker gender preference? ",
-            "What is your caretaker age preference? ",
-            "What language do you primarily speak? ",
-            "Who should the caretaker contact in case of an emergency? (email or phone number) ",
-            "When in the day do you need assistance? (time range) "]
+questions = ["Please describe any medication needs ",
+             "Please describe any transportation needs ",
+             "Please describe any specific needs ",
+             "Please describe any dietary restrictions or preferences",
+             "What hobbies or interests do you have? "]
+
+basic = ["What is your caretaker gender preference? ",
+         "What is your caretaker age preference? ",
+         "What language do you primarily speak? ",
+         "Who should the caretaker contact in case of an emergency? (email or phone number) ",
+         "When in the day do you need assistance? (time range) ",
+         "What city are you located in? "]
 
 print('Hello! We would like to collect some data on your preferences')
 for question in questions:
