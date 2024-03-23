eb8987f21b9fa3c4f3c69446e1bdac4eedd5796b
Clinton Nguyen <clintonnguyen@gmail.com>
2024-03-23 15:58:20 -0700

    added match

---
+++ match.py
@@ -0,0 +1,60 @@
+import csv
+from geopy import distance
+from geopy.geocoders import Nominatim
+order = []
+# info of current user
+e_hobbies = ['singing', 'league']
+e_city = 'folsom'
+e_state = 'ca'
+
+
+def range(city1, state1, city2, state2):
+    geolocator = Nominatim(user_agent="app")
+
+    location1 = f"{city1}, {state1}"
+    location2 = f"{city2}, {state2}"
+
+    location_info1 = geolocator.geocode(location1)
+    location_info2 = geolocator.geocode(location2)
+
+    lat1 = location_info1.latitude
+    lon1 = location_info1.longitude
+    lat2 = location_info2.latitude
+    lon2 = location_info2.longitude
+
+    kms = distance.distance((lat1, lon1), (lat2, lon2))
+    if kms < 49:
+        return 1.0
+    else:
+        return 0.5
+
+
+def match(list1, list2):
+    common = set(list1).intersection(set(list2))
+    common_num = len(common)
+
+    total = set(list1).union(set(list2))
+    total_num = len(total)
+
+    n = common_num / total_num
+    return n
+
+
+with open("users.csv") as users:
+    next(users)
+    read = csv.reader(users)
+
+    for user in read:
+        c_hobbies = user[2].split(", ")
+        c_city = user[3]
+        c_state = user[4]
+
+        range_score = range(e_city, e_state, c_city, c_state)
+        match_score = match(e_hobbies, c_hobbies)
+        gross_score = range_score + match_score
+
+        updated = user
+        updated.append(gross_score)
+        order.append(updated)
+
+print(sorted(order, key=lambda x:x[-1]))
